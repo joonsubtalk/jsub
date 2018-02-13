d42305e10ae637ed4a2809f12fbb77a3cf615d77
JoonSub Chung <jchung2@mediatemple.net>
2018-02-12 23:11:26 -0800

    initial push

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+
+    return (
+        <header className="o-header">
+            <div className="container">
+                sola
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;

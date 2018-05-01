7d9fbbd91610f990150ea3e5c742391ab32f62ec
JoonSub Chung <jchung2@mediatemple.net>
2018-05-01 09:01:58 -0700

    adding desktop mode

---
+++ src/Component/Portfolio/PortfolioDesktop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { info } from '../../configs/info.js';
+
+import { info } from '../../configs/info.js';
+
+class PortfolioDesktop extends Component {
+
+    
+
+    render() {
+        return (
+            <div>s</div>
+        );
+    };
+
+}
+
+export default PortfolioDesktop;
